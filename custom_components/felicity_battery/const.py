cbe656daf5dec7b3348634c21032e09555a1d5db
Vitaly <vitalik33@gmail.com>
2025-12-03 14:11:13 +0200

    Update const.py

--- custom_components/felicity_battery/const.py
+++ custom_components/felicity_battery/const.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from homeassistant.const import Platform
 
-DOMAIN = "felicity_battery"
+DOMAIN = "Felicity"
 
 DEFAULT_PORT = 53970
 DEFAULT_SCAN_INTERVAL = 30  # seconds
